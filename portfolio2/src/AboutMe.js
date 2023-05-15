36b2fef2e0c8f505afe22b85ab41febcb44b7f80
Doyle Jones <doylej1616@gmail.com>
2023-05-15 00:45:35 -0400

    added about me component

---
+++ portfolio2/src/AboutMe.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section id="about" className="about">
+      <div className="container">
+        <h2>About Me</h2>
+        <p>
+          I am a passionate software developer with a strong foundation in
+          web development. I have experience working with various technologies
+          such as HTML, CSS, JavaScript, and React. My goal is to create
+          efficient and user-friendly applications that solve real-world problems.
+        </p>
+        <p>
+          Throughout my journey as a developer, I have collaborated with
+          cross-functional teams, gained valuable experience in agile
+          development methodologies, and delivered high-quality software
+          solutions. I am constantly seeking opportunities to learn and grow,
+          and I thrive in a fast-paced, challenging environment.
+        </p>
+        <p>
+          In addition to my technical skills, I am an excellent communicator
+          and a strong team player. I believe in the power of collaboration and
+          enjoy working with others to achieve shared goals. I am always eager
+          to contribute my skills and expertise to create innovative and
+          impactful solutions.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default About;
