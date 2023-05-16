698d6c4e85aa05cc378bcc569a5b2b61d3714a4a
Doyle Jones <doylej1616@gmail.com>
2023-05-16 08:58:00 -0400

    added a contact component and some css for the component

--- portfolio2/src/App.js
+++ portfolio2/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Home from './Home';
 import Projects from './Projects';
+import Contact from './Contact';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Projects" element={<Projects />} />
+          <Route path="/Contact" element={<Contact />} />
         </Routes>
       </div>
     </Router>
